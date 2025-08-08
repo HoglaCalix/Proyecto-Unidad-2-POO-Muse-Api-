18c14bd74977f9100d7a896092122f3fca166112
HoglaCalix <sarahicalix115@gmail.com>
2025-08-07 21:57:07 -0600

    Addin third pary users

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ from routes.art_routes import router as art_router
 
 
 app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+ 
 app.include_router(art_type_router)
 app.include_router(art_router)
 
